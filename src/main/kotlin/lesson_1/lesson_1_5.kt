a6ad0700ea3878d673766b0166599528671cccdc
t.bukhovets <t.bukhovets@vk.team>
2025-11-10 18:11:59 +0300

    KS-1-5-secondsInSpace, fixed output

--- src/main/kotlin/lesson_1/lesson_1_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_5.kt
@@ -1,11 +1,13 @@
 package lesson_1
 
 fun main() {
-    val totalSeconds = 6480
-    val hours = totalSeconds / 3600
-    val remainingSecondsAfterHours = totalSeconds % 3600
-    val minutes = remainingSecondsAfterHours / 60
-    val seconds = remainingSecondsAfterHours % 60
-    println("$hours:$minutes:$seconds")
 
+    val remainingSecondsAfterHours = TOTAL_SEC_IN_SPACE % 3600
+    val minutesInSpace = remainingSecondsAfterHours / 60
+    val secondsInSpace = remainingSecondsAfterHours % 60
+    val formattedTime = "%02d:%02d:%02d".format(HOURS_IN_SPACE, minutesInSpace, secondsInSpace)
+    println("$formattedTime")
 }
+
+const val TOTAL_SEC_IN_SPACE = 6480
+const val HOURS_IN_SPACE = TOTAL_SEC_IN_SPACE / 3600
