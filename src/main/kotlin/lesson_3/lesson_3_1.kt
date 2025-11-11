6aee7f3724391944c31c660f9fbffc97cdca780d
t.bukhovets <t.bukhovets@vk.team>
2025-11-11 15:47:28 +0300

    KS-3-1-greeting

---
+++ src/main/kotlin/lesson_3/lesson_3_1.kt
@@ -0,0 +1,11 @@
+package lesson_3
+
+fun main() {
+    val userName = "Таня"
+    var greeting: String // Переменная для хранения текста приветствия
+
+    greeting = "Добрый день, $userName!"   // Приветствие для дня
+    println("Дневное приветствие: $greeting")
+    greeting = "Добрый вечер, $userName!"  // Приветствие для вечера
+    println("Вечернее приветствие: $greeting")
+}
