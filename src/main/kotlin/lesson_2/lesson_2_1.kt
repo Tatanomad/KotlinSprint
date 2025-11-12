a6f99a65884babf30cf5c5a058f778d459865729
t.bukhovets <t.bukhovets@vk.team>
2025-11-12 10:11:34 +0300

    KS-2-1-fixed arithmeticMean

--- src/main/kotlin/lesson_2/lesson_2_1.kt
+++ src/main/kotlin/lesson_2/lesson_2_1.kt
@@ -1,10 +1,12 @@
 package lesson_2
 
 fun main() {
-    var grade1: Float = 3.0f //выбрала var, тк если это часть школьного софта, то оценки могут меняться
+    var grade1: Float = 3.0f
     var grade2: Float = 4.0f
     var grade3: Float = 3.0f
     var grade4: Float = 5.0f
-    val arithmeticMean: Float = (grade1 + grade2 + grade3 + grade4) / 4
+    val numberOfGrades = 4
+    val arithmeticMean: Float = (grade1 + grade2 + grade3 + grade4) / numberOfGrades
     println(arithmeticMean)
 }
+
