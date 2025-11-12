c32bd3550d9622fcb13f03260e3cd70e9f0093cc
t.bukhovets <t.bukhovets@vk.team>
2025-11-12 10:24:08 +0300

    KS-2-3-fixed magic numbers

--- src/main/kotlin/lesson_2/lesson_2_3.kt
+++ src/main/kotlin/lesson_2/lesson_2_3.kt
@@ -1,21 +1,24 @@
 package lesson_2
 
 fun main() {
-    var departureHour = 9 //выбрала var, тк время будет меняться
+    var departureHour = 9
     var departureMinute = 39
     var travelDurationInMinutes = 457
 
-    val departureTimeInMinutes = departureHour * 60 + departureMinute // переводим время отправления в минуты
+    val departureTimeInMinutes =
+        departureHour * TOTAL_MINUTES_IN_HOUR + departureMinute // переводим время отправления в минуты
     val arrivalTimeInMinutes = departureTimeInMinutes + travelDurationInMinutes // считаем время прибытия в минутах
     val minutesInADay = 24 * 60 //всего минут в сутках
 
     val actualArrivalMinutesInDay =
         arrivalTimeInMinutes % minutesInADay //учитываем переход через сутки, чтобы не получить, например, такое время прибытия 27:12
 
-    val arrivalHour = actualArrivalMinutesInDay / 60 //час прибытия
-    val arrivalMinute = actualArrivalMinutesInDay % 60 //минуты прибытия
+    val arrivalHour = actualArrivalMinutesInDay / TOTAL_MINUTES_IN_HOUR //час прибытия
+    val arrivalMinute = actualArrivalMinutesInDay % TOTAL_MINUTES_IN_HOUR //минуты прибытия
     val formattedTime = "%02d:%02d".format(arrivalHour, arrivalMinute)
 
     println("$formattedTime")
 
 }
+
+const val TOTAL_MINUTES_IN_HOUR = 60
