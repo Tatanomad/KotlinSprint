340a941c8ca01dae06b37fb5cfa5f5adab372e16
t.bukhovets <t.bukhovets@vk.team>
2025-11-14 13:29:28 +0300

    KS-2-4-buff, fixed constants

--- src/main/kotlin/lesson_2/lesson_2_4_buff.kt
+++ src/main/kotlin/lesson_2/lesson_2_4_buff.kt
@@ -3,11 +3,13 @@ package lesson_2
 fun main() {
     var crystallineOre = 7
     var ironOre = 11
-    val bonusPercent = 20 // Размер бонуса в процентах
+    val bonusPercent = 20
 
-    val crystalBonus = (crystallineOre * bonusPercent) / 100  // Расчет бонусного количества кристаллической руды
-    val ironBonus = (ironOre * bonusPercent) / 100 // Расчет бонусного количества железной руды
+    val crystalBonus = (crystallineOre * bonusPercent) / CONVERTING_TO_A_PERCENTAGE
+    val ironBonus = (ironOre * bonusPercent) / CONVERTING_TO_A_PERCENTAGE
 
     println("Бонусная кристаллическая руда: $crystalBonus")
     println("Бонусная железная руда: $ironBonus")
 }
+
+const val CONVERTING_TO_A_PERCENTAGE = 100
