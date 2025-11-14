1d952ef50757d9d4be498881e1a4044041b61b98
t.bukhovets <t.bukhovets@vk.team>
2025-11-14 13:58:44 +0300

    KS-3-1-greeting, fix variable

--- src/main/kotlin/lesson_3/lesson_3_1.kt
+++ src/main/kotlin/lesson_3/lesson_3_1.kt
@@ -2,10 +2,8 @@ package lesson_3
 
 fun main() {
     val userName = "Таня"
-    var greeting: String // Переменная для хранения текста приветствия
-
-    greeting = "Добрый день, $userName!" // Приветствие для дня
+    var greeting = "Добрый день, $userName!"
     println("Дневное приветствие: $greeting")
-    greeting = "Добрый вечер, $userName!" // Приветствие для вечера
+    greeting = "Добрый вечер, $userName!"
     println("Вечернее приветствие: $greeting")
 }
