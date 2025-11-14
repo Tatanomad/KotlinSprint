45c77c6bc4aa9d8b06662ce448dae6cc5d3e15b7
t.bukhovets <t.bukhovets@vk.team>
2025-11-14 13:14:49 +0300

    KS-1-5-fixed constants

--- src/main/kotlin/lesson_1/lesson_1_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_5.kt
@@ -2,12 +2,10 @@ package lesson_1
 
 fun main() {
     val totalSecInSpace = 6480
-    val hoursInSpace = totalSecInSpace / TOTAL_SEC_IN_HOUR  //считаем количество часов в космосе
-    val remainingSecondsAfterHours =
-        totalSecInSpace % TOTAL_SEC_IN_HOUR  //записываем остаток секунд после вычисления часов
-    val minutesInSpace =
-        remainingSecondsAfterHours / TOTAL_SEC_IN_MINUTE  //считаем целое количество минут в космосе из оставшихся секунд
-    val secondsInSpace = remainingSecondsAfterHours % TOTAL_SEC_IN_MINUTE  //записываем остаток секунд после деления
+    val hoursInSpace = totalSecInSpace / TOTAL_SEC_IN_HOUR
+    val remainingSecondsAfterHours = totalSecInSpace % TOTAL_SEC_IN_HOUR
+    val minutesInSpace = remainingSecondsAfterHours / TOTAL_SEC_IN_MINUTE
+    val secondsInSpace = remainingSecondsAfterHours % TOTAL_SEC_IN_MINUTE
     val formattedTime = "%02d:%02d:%02d".format(hoursInSpace, minutesInSpace, secondsInSpace)
     println("$formattedTime")
 }
