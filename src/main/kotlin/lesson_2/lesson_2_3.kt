8826286a8ee71d022dbb1500e7c5e92d3c871125
t.bukhovets <t.bukhovets@vk.team>
2025-11-14 13:23:55 +0300

    KS-2-3-train, fixed constants

--- src/main/kotlin/lesson_2/lesson_2_3.kt
+++ src/main/kotlin/lesson_2/lesson_2_3.kt
@@ -5,20 +5,16 @@ fun main() {
     var departureMinute = 39
     var travelDurationInMinutes = 457
 
-    val departureTimeInMinutes =
-        departureHour * TOTAL_MINUTES_IN_HOUR + departureMinute // переводим время отправления в минуты
-    val arrivalTimeInMinutes = departureTimeInMinutes + travelDurationInMinutes // считаем время прибытия в минутах
-    val minutesInADay = 24 * 60 //всего минут в сутках
-
-    val actualArrivalMinutesInDay =
-        arrivalTimeInMinutes % minutesInADay //учитываем переход через сутки, чтобы не получить, например, такое время прибытия 27:12
-
-    val arrivalHour = actualArrivalMinutesInDay / TOTAL_MINUTES_IN_HOUR //час прибытия
-    val arrivalMinute = actualArrivalMinutesInDay % TOTAL_MINUTES_IN_HOUR //минуты прибытия
+    val departureTimeInMinutes = departureHour * TOTAL_MINUTES_IN_HOUR + departureMinute
+    val arrivalTimeInMinutes = departureTimeInMinutes + travelDurationInMinutes
+    val minutesInADay = TOTAL_HOURS_IN_A_DAY * TOTAL_MINUTES_IN_HOUR
+    val actualArrivalMinutesInDay = arrivalTimeInMinutes % minutesInADay
+    val arrivalHour = actualArrivalMinutesInDay / TOTAL_MINUTES_IN_HOUR
+    val arrivalMinute = actualArrivalMinutesInDay % TOTAL_MINUTES_IN_HOUR
     val formattedTime = "%02d:%02d".format(arrivalHour, arrivalMinute)
 
     println("$formattedTime")
-
 }
 
 const val TOTAL_MINUTES_IN_HOUR = 60
+const val TOTAL_HOURS_IN_A_DAY = 24
