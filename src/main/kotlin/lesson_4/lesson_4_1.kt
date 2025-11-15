f62188b2c10713635f5ff8503b7f33669bfebc7c
t.bukhovets <t.bukhovets@vk.team>
2025-11-15 10:13:16 +0300

    KS-4-1-restaurant reservation, fixed var

--- src/main/kotlin/lesson_4/lesson_4_1.kt
+++ src/main/kotlin/lesson_4/lesson_4_1.kt
@@ -1,8 +1,8 @@
 package lesson_4
 
 fun main() {
-    var bookedToday = 13
-    var bookedTomorrow = 9
+    val bookedToday = 13
+    val bookedTomorrow = 9
     val availableToday: Boolean = bookedToday < TOTAL_TABLES
     val availableTomorrow: Boolean = bookedTomorrow < TOTAL_TABLES
     val reservationResult = "[Доступность столиков на сегодня: $availableToday],\n" +
