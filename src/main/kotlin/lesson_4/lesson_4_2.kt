64811b2da4acc1ef6e4da70f5bd5fb7f19e237c0
t.bukhovets <t.bukhovets@vk.team>
2026-01-21 11:05:30 +0300

    KS-4-2-поправлен вывод в println, добавлена проверка еще одного груза, убрала скобки

--- src/main/kotlin/lesson_4/lesson_4_2.kt
+++ src/main/kotlin/lesson_4/lesson_4_2.kt
@@ -1,10 +1,14 @@
 package lesson_4
 
 fun main() {
-    val weight = 50
-    val volume = 100
-    val comparisonResult = (weight > MIN_WEIGHT && weight <= MAX_WEIGHT) && (volume < MAX_VOLUME)
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории \'Average\':$comparisonResult")
+    val weight = 20
+    val volume = 80
+
+    val weight2 = 50
+    val volume2 = 100
+
+    println("Груз с весом $weight кг и объемом $volume л соответствует категории \'Average\':${weight > MIN_WEIGHT && weight <= MAX_WEIGHT && volume < MAX_VOLUME}")
+    println("Груз с весом $weight2 кг и объемом $volume2 л соответствует категории \'Average\':${weight2 > MIN_WEIGHT && weight2 <= MAX_WEIGHT && volume2 < MAX_VOLUME}")
 }
 
 const val MIN_WEIGHT = 35
