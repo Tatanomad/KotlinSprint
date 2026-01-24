0feb7819640bb635b26e48b86b67a3f3f8d1d4e5
t.bukhovets <t.bukhovets@vk.team>
2026-01-24 13:00:56 +0300

    KS-5-1-Authorization

---
+++ src/main/kotlin/lesson_5/lesson_5_1.kt
@@ -0,0 +1,10 @@
+package lesson_5
+
+fun main() {
+    val result = readln().toInt()
+    println("Сколько будет 2+2?")
+    if (result == 4)
+        println("Добро пожаловать!")
+    else
+        println("Доступ запрещен")
+}
