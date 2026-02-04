bc6503495bf7b8a5483cb4fefa2289b4d074598d
t.bukhovets <t.bukhovets@vk.team>
2026-02-04 14:00:33 +0300

    KS-7-1-вынесла создание диапазонов за пределы цикла

--- src/main/kotlin/lesson_7/lesson_7_1.kt
+++ src/main/kotlin/lesson_7/lesson_7_1.kt
@@ -1,13 +1,14 @@
 package lesson_7
 
 fun main() {
-
+    val range1 = 'a'..'z'
+    val range2 = 0..9
     var password = ""
     for (i in 0..5) {
         if (i % 2 == 0) {
-            password += ('a'..'z').random()
+            password += range1.random()
         } else {
-            password += (0..9).random()
+            password += range2.random()
         }
     }
     println("Ваш шестизначный пароль: $password")
