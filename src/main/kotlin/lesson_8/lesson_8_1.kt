139b1f504ba9883d67294a3b995a06da9b622453
t.bukhovets <t.bukhovets@vk.team>
2026-02-07 13:21:46 +0300

    KS-8-1-ads

---
+++ src/main/kotlin/lesson_8/lesson_8_1.kt
@@ -0,0 +1,7 @@
+package lesson_8
+
+fun main() {
+    val viewsPerDay = arrayOf(1000, 1200, 780, 1055, 999, 456, 1290)
+    val sumOfWeek = viewsPerDay.sum()
+    println("Сумма просмотров за неделю: $sumOfWeek")
+}
