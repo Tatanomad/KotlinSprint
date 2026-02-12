b19d842c47452a185223f3e76da3ab7e39de4d9e
t.bukhovets <t.bukhovets@vk.team>
2026-02-12 09:43:58 +0300

    KS-8-5-поправила диапазон индексов i in arrayOfIngridients.indices

--- src/main/kotlin/lesson_8/lesson_8_5.kt
+++ src/main/kotlin/lesson_8/lesson_8_5.kt
@@ -4,7 +4,7 @@ fun main() {
     println("Сколько ингридиентов будет в вашем рецепте?")
     val ammountOfIngridients = readln().toInt()
     val arrayOfIngridients = Array(ammountOfIngridients) { "" }
-    for (i in 0 until ammountOfIngridients) {
+    for (i in arrayOfIngridients.indices) {
         println("Введите ингридиент")
         val newIngridient = readln()
         arrayOfIngridients[i] = newIngridient
