8b20a29b5699d23335a56f946bce4790dc3d9c6c
t.bukhovets <t.bukhovets@vk.team>
2026-02-19 11:28:41 +0300

    KS-10-5-разделила логин и генерацию токена

--- src/main/kotlin/lesson_10/lesson_10_5.kt
+++ src/main/kotlin/lesson_10/lesson_10_5.kt
@@ -5,25 +5,23 @@ fun main() {
     val inputLogin = readln()
     println("Введите пароль")
     val inputPassword = readln()
-    val token = authorizationToken(inputLogin, inputPassword)
-    if (token != null) {
+    if (inputLogin == LOGIN && inputPassword == PASSWORD) {
+        val token = createToken(inputLogin, inputPassword)
         getCart(token)
-    } else println("Ошибка авторизации")
-
+    } else
+        println("Ошибка авторизации")
 }
 
 const val LOGIN = "Harry Potter"
 const val PASSWORD = "12345"
 
-fun authorizationToken(inputLogin: String, inputPassword: String): String? {
-    if (inputLogin == LOGIN && inputPassword == PASSWORD) {
-        val range1 = 'a'..'z'
-        val range2 = 0..9
-        return (range1.toList() + range2.toList()).shuffled<Any>().toString()
-    } else return null
+fun createToken(inputLogin: String, inputPassword: String): String? {
+    val range1 = 'a'..'z'
+    val range2 = 0..9
+    return (range1.toList() + range2.toList()).shuffled<Any>().toString()
 }
 
-fun getCart(token: String) {
+fun getCart(token: String?) {
     val shoppingCart = listOf("Платье арт.123", "Шорты арт.345", "Футболка арт.789")
     return shoppingCart.forEach { println(it) }
 }
