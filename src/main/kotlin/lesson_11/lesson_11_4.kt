1641c64fd8439230176a367f06f8b8aa900c2393
t.bukhovets <t.bukhovets@vk.team>
2026-02-19 16:48:27 +0300

    KS-11-4-recipes_app

---
+++ src/main/kotlin/lesson_11/lesson_11_4.kt
@@ -0,0 +1,25 @@
+package lesson_11
+
+class RecipeCategory(
+    val id:Int,
+    val categoryTitle: String,
+    val imageUrl: String,
+    val recipeTitle: String,
+    val recipeImageUrl: String,
+
+)
+class Recipe (
+    val id: Int,
+    val idCategory: Int,
+    val title:String,
+    val imageUrl: String,
+    val ingredients:List<String>,
+    val numberOfPortions: Int,
+    val mealPrep:String,
+)
+
+class Ingredient(
+    val ingredientName:String,
+    val amount:Int,
+    val unitOfMeasure: String,
+)
