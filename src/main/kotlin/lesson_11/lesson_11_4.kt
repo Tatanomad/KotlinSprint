7c6971d8a3faf3fe2554d5aba0b07a956949e2de
t.bukhovets <t.bukhovets@vk.team>
2026-02-21 09:34:34 +0300

    KS-11-4-убрала свойство Категория из класса Рецепт, поправила типы данных List<Recipe> и List<Ingredient>

--- src/main/kotlin/lesson_11/lesson_11_4.kt
+++ src/main/kotlin/lesson_11/lesson_11_4.kt
@@ -1,26 +1,25 @@
 package lesson_11
 
 class RecipeCategory(
-    val id:Int,
+    val id: Int,
     val categoryTitle: String,
     val imageUrl: String,
-    val recipeTitle: String,
-    val recipeImageUrl: String,
+    val recipes: List<Recipe>,
 
-)
-class Recipe (
+    )
+
+class Recipe(
     val id: Int,
-    val idCategory: Int,
-    val title:String,
+    val title: String,
     val imageUrl: String,
-    val ingredients:List<String>,
+    val ingredients: List<Ingredient>,
     val numberOfPortions: Int,
-    val mealPrep:String,
-    val addToFavourite:Boolean,
+    val mealPrep: String,
+    val addToFavourite: Boolean,
 )
 
 class Ingredient(
-    val ingredientName:String,
-    val amount:Int,
+    val ingredientName: String,
+    val amount: Int,
     val unitOfMeasure: String,
 )
