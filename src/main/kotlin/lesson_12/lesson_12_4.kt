87b5f687bcb9a27599d76adc8226cfc1b0aa4b80
t.bukhovets <t.bukhovets@vk.team>
2026-02-25 15:20:44 +0300

    KS-12-4-поправила замечания: переменные объявила в primary constructor,  присвоила значение при инициализации в init блоке

--- src/main/kotlin/lesson_12/lesson_12_4.kt
+++ src/main/kotlin/lesson_12/lesson_12_4.kt
@@ -1,18 +1,19 @@
 package lesson_12
 
-class WeatherEveryDay(dayTemp: Int, nightTemp: Int, _hasPrecipitation: Boolean) {
+class WeatherEveryDay(var dayTemp: Int, var nightTemp: Int, var hasPrecipitation: Boolean) {
 
-    var dayTemp = dayTemp - CELSIUS_TO_KELVIN
-    var nightTemp = nightTemp - CELSIUS_TO_KELVIN
-    var hasPrecipitation = _hasPrecipitation
+    var dayKelvin:Int = 0
+    var nightKelvin:Int = 0
 
     init {
+        dayKelvin = dayTemp - CELSIUS_TO_KELVIN
+        nightKelvin = nightTemp - CELSIUS_TO_KELVIN
         printWeather()
     }
 
     fun printWeather() {
         val precipitationText = if (hasPrecipitation) "Ожидаются" else "Не ожидаются"
-        println("Погода: днем $dayTemp°C, ночью $nightTemp°C. Осадки: $precipitationText")
+        println("Погода: днем ${dayKelvin}°C, ночью ${nightKelvin}°C. Осадки: $precipitationText")
     }
 }
 
