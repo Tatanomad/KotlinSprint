f3160ed85064082d54b5255e86324840278b855e
t.bukhovets <t.bukhovets@vk.team>
2026-02-27 15:44:21 +0300

    KS-13-2-сделала поля класса приватными, вернула пробелы в println, поменяла название класса

--- src/main/kotlin/lesson_13/lesson_13_2.kt
+++ src/main/kotlin/lesson_13/lesson_13_2.kt
@@ -1,16 +1,16 @@
 package lesson_13
 
-class PhoneBook(
-    val name: String,
-    val phoneNumber: Long,
-    val company: String? = null,
+class Contact(
+    private val name: String,
+    private val phoneNumber: Long,
+    private val company: String? = null,
 ) {
     fun printContactInfo() {
-        println("Имя:$name, Номер телефона:$phoneNumber, Компания: ${company ?: "<не указано>"}")
+        println("Имя: $name, Номер телефона: $phoneNumber, Компания: ${company ?: "<не указано>"}")
     }
 }
 
 fun main() {
-    val phoneBook = PhoneBook("Ростислав", 89123456789)
+    val phoneBook = Contact("Ростислав", 89123456789)
     phoneBook.printContactInfo()
 }
