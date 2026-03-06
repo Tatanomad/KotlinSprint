02e2261195001e205731ae0e621f5cd35f60c61f
t.bukhovets <t.bukhovets@vk.team>
2026-03-06 09:52:20 +0300

    KS-16-2-round

---
+++ src/main/kotlin/lesson_16/lesson_16_2.kt
@@ -0,0 +1,14 @@
+package lesson_16
+
+private class Round(val radius: Double) {
+    private val pi: Double = 3.14
+    fun circumference() = 2 * pi * radius
+    fun circleArea() = pi * radius * radius
+}
+
+fun main() {
+    val formula = Round(15.0)
+    val circumference = formula.circumference()
+    val area = formula.circleArea()
+    println("Длина окружности равна $circumference. Площадь круга: $area")
+}
